1eb0651fe61273fde5ddd2bea37f9ec98d12d89c
Lukas Eipert <git@leipert.io>
2015-08-07 05:10:15 +0200

    Reenabled curl

--- increaseVersion.sh
+++ increaseVersion.sh
@@ -17,14 +17,14 @@ git checkout master
 git push
 git checkout gh-pages
 echo v${NAME} > index.html
-git commit -am "Bumped version to ${NAME}"
+git commit -am "Bumped version to v${NAME}"
 git push
 git checkout master
 
 source .secret.sh .
 
 # Trigger by Source branch named staging
-# curl -H "Content-Type: application/json" --data '&#123;"source_type": "Branch", "source_name": "master"&#125;' -X POST https://registry.hub.docker.com/u/leipert/atom-apm/trigger/${SECRET}/
+curl -H "Content-Type: application/json" --data '&#123;"source_type": "Branch", "source_name": "master"&#125;' -X POST https://registry.hub.docker.com/u/leipert/atom-apm/trigger/${SECRET}/
 #
 # # Trigger by Source branch named staging
-# curl -H "Content-Type: application/json" --data $BRANCH_VERSION -X POST https://registry.hub.docker.com/u/leipert/atom-apm/trigger/${SECRET}/
+curl -H "Content-Type: application/json" --data $BRANCH_VERSION -X POST https://registry.hub.docker.com/u/leipert/atom-apm/trigger/${SECRET}/
