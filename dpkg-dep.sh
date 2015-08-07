222bc6f1d0fe98686f54a93a02ea0eb327a9ffa1
Lukas Eipert <git@leipert.io>
2015-08-07 04:33:26 +0200

    Initial gh pages

--- dpkg-dep.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-echo "Installing $1"
-dpkg -i $1 > /dev/null 2>&1
-if [ $? -gt 0 ]; then
-  echo "Installing $1 dependencies"
-  DEBIAN_FRONTEND=noninteractive apt-get -fyqq install --no-install-recommends > /dev/null
-fi
--- env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-NAME=1.0.5
-BRANCH=v1.0.x
-BRANCH_VERSION='&#123;{"source_type":"Branch","source_name":"v1.0.x"}&#125;'
-FILE_NAME=atom-amd64.deb
-URL=https://github.com/atom/atom/releases/download/v1.0.5/atom-amd64.deb
