b4e213904acbdc560fba359acb10186ec05a12bd
Lukas Eipert <git@leipert.io>
2015-10-08 08:08:08 +0200

    Bumped version to 1.0.19

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-NAME=1.0.18
+NAME=1.0.19
 BRANCH=v1.0.x
 BRANCH_VERSION='&#123;{"source_type":"Branch","source_name":"v1.0.x"}&#125;'
 FILE_NAME=atom-amd64.deb
-URL=https://github.com/atom/atom/releases/download/v1.0.18/atom-amd64.deb
+URL=https://github.com/atom/atom/releases/download/v1.0.19/atom-amd64.deb
