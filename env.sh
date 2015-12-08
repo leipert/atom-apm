ab7b56a5b9328e96525de014017c7d06a79eda1c
Lukas Eipert <git@leipert.io>
2015-12-08 09:26:38 +0100

    Bumped version to 1.2.4

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-NAME=1.1.0
-BRANCH=v1.1.x
-BRANCH_VERSION='&#123;{"source_type":"Branch","source_name":"v1.1.x"}&#125;'
+NAME=1.2.4
+BRANCH=v1.2.x
+BRANCH_VERSION='&#123;{"source_type":"Branch","source_name":"v1.2.x"}&#125;'
 FILE_NAME=atom-amd64.deb
-URL=https://github.com/atom/atom/releases/download/v1.1.0/atom-amd64.deb
+URL=https://github.com/atom/atom/releases/download/v1.2.4/atom-amd64.deb
